0d72dc2555c571017ee030557d9e521f784ea538
Smiti <mishra.smiti06@gmail.com>
2021-09-22 16:00:28 -0400

    updated movement

--- client.js
+++ client.js
@@ -3,19 +3,26 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "135.23.223.133",
-    port: 50542
+    port: 50542,
+    
   });
   conn.on("connect", (connect) => {
     // code that does something when the connection is first established
     conn.write("Name: SM");
     conn.write("Say: Hey");
     console.log("Successfully connected to game server");
+    //conn.write("Move: left");
+    conn.write("Move: down");
+    
     console.log("hi connected!!!...");
     
   });
   
   conn.on('data', (data) => {
-    
+    // 
+
+    // conn.write("Move: up");
+    // conn.write("Move: left");
     console.log("DATA CAME IN!!!!!");
    
 });
