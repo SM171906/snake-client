37d4ce246b485446d6e5076cd008bc35d749ba9a
Smiti <mishra.smiti06@gmail.com>
2021-09-22 17:08:48 -0400

    created new file and updated it with setup &handle function

--- play.js
+++ play.js
@@ -3,21 +3,10 @@ const stdin = process.stdin;
 // establishes a connection with the game server
 
 
-const {connect} = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
-  
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = function () {
-  // your code here
-  // \u0003 maps to ctrl+c input
-if (key === '\u0003') {
-  process.exit();
-}
-};
+console.log("Connecting ...");
+connect();
+
+setupInput();
