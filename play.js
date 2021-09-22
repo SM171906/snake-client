78ba5d7899ef28d0a9b3deb94c192580d1a76a3b
Smiti <mishra.smiti06@gmail.com>
2021-09-22 17:05:31 -0400

    updated command to cancle the game

--- play.js
+++ play.js
@@ -1,8 +1,23 @@
 const net = require("net");
-
+const stdin = process.stdin;
 // establishes a connection with the game server
 
 
 const {connect} = require("./client");
 
-
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = function () {
+  // your code here
+  // \u0003 maps to ctrl+c input
+if (key === '\u0003') {
+  process.exit();
+}
+};
