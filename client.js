177d47b0e0eead665c084d3083d78168ec973891
Smiti <mishra.smiti06@gmail.com>
2021-09-22 14:37:03 -0400

    updated the name and message

--- client.js
+++ client.js
@@ -1,15 +1,21 @@
 const net = require("net");
-
+//const stdin = process.stdin;
 const connect = function () {
   const conn = net.createConnection({
     host: "135.23.223.133",
     port: 50542
   });
-  conn.on("connect", () => {
+  conn.on("connect", (connect) => {
     // code that does something when the connection is first established
+    conn.write("Name: SM");
+    conn.write("Say: Hey");
+    console.log("Successfully connected to game server");
     console.log("hi connected!!!...");
+    
   });
+  
   conn.on('data', (data) => {
+    
     console.log("DATA CAME IN!!!!!");
    
 });
